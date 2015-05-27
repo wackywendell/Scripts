6e8b9ec56d6441ced656f379356018661542701e
Wendell Smith <wackywendell@gmail.com>
2015-05-27 11:05:19 -0400

    Added previously existing scripts

---
+++ git-author-rewrite.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="wendellwsmith@gmail.com"
+CORRECT_NAME="Wendell Smith"
+CORRECT_EMAIL="wackywendell@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
---
+++ startup.m
@@ -0,0 +1,7 @@
+format compact;
+set(0, 'defaultlinelinewidth', 2);
+set(0, 'DefaultFigurePosition', [360, 278, 400, 300]);
+feature('DefaultCharacterSet', 'UTF8');
+feature('DefaultCharacterSet');
+
+% Some unicode characters: pi="?", theta="?"
